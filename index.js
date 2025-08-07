1c9d40040478cb7369e6af577910dcb3908368c6
Noel Osarinmwian <osarinmwian@bepeerless.co>
2025-08-07 07:18:58 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Auto-initialization requires API key
-import NexusInsight from './lib/index.js';
-
-// Export class only - requires API key for initialization
-export default NexusInsight;
-
-// Helper function to create instance with API key
-export const createAnalytics = (apiKey: string) => {
-  return new NexusInsight({ apiKey });
-};
+// Direct export from source
+export { default } from './src/index';
+export * from './src/index';
